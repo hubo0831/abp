314c2b87eb81d7e0b2d4907c36630d89d5ee0364
Hubo <hubo0831@qq.com>
2019-10-22 14:38:20 +0800

    完善Quartz调度扩展

--- framework/src/Volo.Abp.BackgroundJobs.Quartz/Volo/Abp/BackgroundJobs/Quartz/AbpQuartzJobExtensions.cs
+++ framework/src/Volo.Abp.BackgroundJobs.Quartz/Volo/Abp/BackgroundJobs/Quartz/AbpQuartzJobExtensions.cs
@@ -26,9 +26,8 @@ namespace Volo.Abp.BackgroundJobs.Quartz
         {
             await ScheduleRealTimeJob<TJob>(context.Scheduler, data);
         }
-        /// <summary>调度实时任务</summary>
-        public static async Task ScheduleRealTimeJob<TJob>(this IScheduler scheduler, object data = null, Action<ITrigger, IJobDetail> action = null)
-            where TJob : IJob
+        /// <summary>获得实时触发器</summary>
+        public static ITrigger GetRealTimeTrigger(this IScheduler scheduler, Action<ITrigger> action = null)
         {
             var newJobIndex = Interlocked.Increment(ref m_NewJobIndex).ToString();
             var name = $"{AbpQuartzConsts.RealTimeJob}:{newJobIndex}";
@@ -36,8 +35,28 @@ namespace Volo.Abp.BackgroundJobs.Quartz
                 .WithIdentity(name, AbpQuartzConsts.TriggerGroup)
                 .StartNow()
                 .Build();
+            action?.Invoke(trigger);
+            return trigger;
+        }
+        /// <summary>获得某个时间调度的触发器</summary>
+        public static ITrigger GetScheduleTimeTrigger(this IScheduler scheduler, DateTime scheduleTime, Action<ITrigger> action = null)
+        {
+            var newJobIndex = Interlocked.Increment(ref m_NewJobIndex).ToString();
+            var name = $"{AbpQuartzConsts.ScheduleJob}:{newJobIndex}";
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity(name, AbpQuartzConsts.TriggerGroup)
+                .StartAt(scheduleTime.ToUniversalTime())
+                .Build();
+            action?.Invoke(trigger);
+            return trigger;
+        }
+        /// <summary>调度实时任务</summary>
+        public static async Task ScheduleRealTimeJob<TJob>(this IScheduler scheduler, object data = null, Action<ITrigger, IJobDetail> action = null)
+            where TJob : IJob
+        {
+            var trigger = scheduler.GetRealTimeTrigger();
             var job = JobBuilder.Create<TJob>()
-                .WithIdentity(name, AbpQuartzConsts.JobGroup)
+                .WithIdentity(trigger.Key.Name, AbpQuartzConsts.JobGroup)
                 .UsingJobDataIgnoreIfNull(data)
                 .Build();
             action?.Invoke(trigger, job);
@@ -54,17 +73,19 @@ namespace Volo.Abp.BackgroundJobs.Quartz
         public static async Task ScheduleJob<TJob>(this IScheduler scheduler, DateTime scheduleTime, object data = null, Action<ITrigger, IJobDetail> action = null)
             where TJob : IJob
         {
-            var newJobIndex = Interlocked.Increment(ref m_NewJobIndex).ToString();
-            var name = $"{AbpQuartzConsts.ScheduleJob}:{newJobIndex}";
-            var trigger = TriggerBuilder.Create()
-                .WithIdentity(name, AbpQuartzConsts.TriggerGroup)
-                .StartAt(scheduleTime.ToUniversalTime())
-                .Build();
+            var trigger = scheduler.GetScheduleTimeTrigger(scheduleTime);
             var job = JobBuilder.Create<TJob>()
-                .WithIdentity(name, AbpQuartzConsts.JobGroup)
+                .WithIdentity(trigger.Key.Name, AbpQuartzConsts.JobGroup)
                 .UsingJobDataIgnoreIfNull(data)
                 .Build();
             action?.Invoke(trigger, job);
+            await scheduler.ScheduleJob(job, trigger);
+        }
+        /// <summary>调度任务</summary>
+        public static async Task ScheduleJob(this IScheduler scheduler, DateTime scheduleTime, JobKey jobKey, Action<ITrigger> action = null)
+        {
+            var trigger = scheduler.GetScheduleTimeTrigger(scheduleTime, action);
+            var job = await scheduler.GetJobDetail(jobKey);
             await scheduler.ScheduleJob(job, trigger);
         }
         /// <summary>加入任务数据</summary>
